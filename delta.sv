eb62b0efe16d87bd1c6dd01294165edba38c002d
MoonLoone <mmaks02@yandex.ru>
2023-05-30 13:23:18 +0300

    Works. Need to make decisions

--- delta.sv
+++ delta.sv
@@ -1,24 +1,36 @@
 module delta(
 input logic [2:0] delta,
-inout logic [3:0] count,
-inout logic [3:0] current,
-logic clk, resetn, direction
+output logic [3:0] count,
+output logic [3:0] current,
+input logic clk,
+input logic resetn,
+output logic direction
 );
-always_ff(posedge clk or negedge resetn)
-if (direction)
+always_ff @(posedge clk or negedge resetn)
+begin
+if (!resetn) begin
+current <= 0;
+count <= 0;
+direction <= 1;
+end
+else if (direction)
 	if (current == delta) 
 		begin
-		current = current-1;
-		count = count - 1;
-		direction = 0;
+		current <= current - 1;
+		count <= count - 1;
+		direction <= 0;
 		end
 	else begin
-		count = count+1;
-		count = count+1;
+		count <= count+1;
+		current <= current+1;
 		end
-else if (current == delta-1) begin
-current = current+1;
-count = count + 1;
-direction = 1;
+else if (current == 1) begin
+	count <= count + 1;
+	direction <= 1;
+	end
+	else begin
+		count <= count-1;
+		current <= current-1;
+	end
 end
 endmodule
